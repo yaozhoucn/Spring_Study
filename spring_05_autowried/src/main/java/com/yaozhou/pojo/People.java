11ed34a338fb20bc24e54e106dfd3ff794cafac7
yaozhoucn <wangxiao_h@126.com>
2021-07-07 18:27:10 +0800

    autowired

--- spring_05_autowried/src/main/java/com/yaozhou/pojo/People.java
+++ spring_05_autowried/src/main/java/com/yaozhou/pojo/People.java
@@ -3,6 +3,7 @@ package com.yaozhou.pojo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Created by WXHang on HANG at 2021/7/7 17:40
@@ -12,7 +13,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class People {
+
     private String name;
+    @Autowired
     private Dog dog;
+    @Autowired
     private Cat cat;
 }
